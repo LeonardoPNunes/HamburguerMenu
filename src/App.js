c1d64cd24fa4fa324a116caa2be407eda03dc229
Leonardo Nunes <zleo465@gmail.com>
2022-01-31 21:28:30 -0200

    Created hamburguerMenu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
-function App() {
+export default function App() {
+
+  const[active,setActive] = useState(false)
+
+  const toogleMode = () =>{
+    setActive(!active)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App">
+      <div 
+       className={active ? "icon icon-active" : "icon"}
+       onClick={toogleMode}
+      >
+        <div className="hamburguer hamburguer-icon"></div>
+      </div>
+      <div className={active ? "menu menu-open" : "menu menu-close"}>
+        <nav className="list">
+          <ul className="list-items">
+            <li>home</li>
+            <li>product</li>
+            <li>contact</li>
+            <li>about us</li>
+          </ul>
+        </nav>
+      </div> 
+    </header>
   );
 }
-
-export default App;
